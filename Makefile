dd4150c05814dffc17190ce0b493ded95201696f
Armanit Garg <armanit.garg@gmail.com>
2020-08-16 00:18:13 +0530

    grpc testing

--- Makefile
+++ Makefile
@@ -1,23 +1,3 @@
-# proto:
-# 	@ if ! which protoc > /dev/null; then \
-# 		echo "error: protoc not installed" >&2; \
-# 		exit 1; \
-# 	fi
-# 	@ if ! which protoc-gen-go > /dev/null; then \
-# 		echo "error: protoc-gen-go not installed" >&2; \
-# 		exit 1; \
-# 	fi
-
-# 	@ echo Compiling Protobufs
-# 	@ for file in $$(git ls-files '*.proto'); do \
-# 		protoc \
-# 		--go_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:. \
-# 		--go-grpc_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:. \
-# 		--go_opt=paths=source_relative \
-# 		--go-grpc_opt=paths=source_relative \
-# 		$$file; \
-# 	done
-
 .PHONY:	build run
 
 build:
@@ -30,5 +10,17 @@ run:
 	kubectl get po -w
 	kubectl logs diago-0 -f
 
+.PHONY: local
 local:
-	go build cmd/main.go
+	go build -o ser cmd/server/lol.go
+	go build -o cle cmd/server/cle.go
+
+PROTOC := protoc
+
+.PHONY: proto
+proto:
+
+	$(PROTOC) \
+		--go_out=Mgrpc/service_config/service_config.proto=/proto-gen/api:. \
+		--go-grpc_out=Mgrpc/service_config/service_config.proto=/proto-gen/api:. \
+		api/proto/lol.proto
---
+++ cmd/server/lol.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"time"
+
+	pb "github.com/t-bfame/diago/proto-gen/lol"
+	"google.golang.org/grpc"
+)
+
+type routeGuideServer struct {
+	pb.UnimplementedRouteGuideServer
+}
+
+func randomPoint(r *rand.Rand) *pb.Point {
+	lat := (r.Int31n(180) - 90) * 1e7
+	long := (r.Int31n(360) - 180) * 1e7
+	return &pb.Point{Latitude: lat, Longitude: long}
+}
+
+func (s *routeGuideServer) RouteChat(stream pb.RouteGuide_RouteChatServer) error {
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	go func() {
+		for {
+			if err := stream.Send(randomPoint(r)); err != nil {
+				break
+			}
+		}
+	}()
+
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(in)
+	}
+}
+
+func newServer() *routeGuideServer {
+	s := &routeGuideServer{}
+	return s
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
