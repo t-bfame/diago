e2f7e16f9ab6f8640148faf66d199af5daa097d7
rmn7 <armanit.garg@gmail.com>
2021-01-18 12:07:09 +0530

    upd: default api + ui port to 80

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 type Config struct {
 	Host           string `envconfig:"DIAGO_HOST" required:"true"`
 	GRPCPort       uint64 `envconfig:"DIAGO_GRPC_PORT" default:"5000"`
-	APIPort        uint64 `envconfig:"DIAGO_API_PORT" default:"9001"`
+	APIPort        uint64 `envconfig:"DIAGO_API_PORT" default:"80"`
 	PrometheusPort uint64 `envconfig:"DIAGO_PROMETHEUS_PORT" default:"2112"`
 
 	DefaultGroupCapacity uint64 `envconfig:"DIAGO_DEFAULT_GROUP_CAPACITY" default:"20"`
